9bdd4688109026a84017ae9ea6d2125cdd9d5938
汪志鹏 <13476859295@163.com>
2023-02-10 18:56:00 +0800

    Create tree.js

---
+++ client/src/tree.js
@@ -0,0 +1,115 @@
+export default {
+    module: "react-ui-tree",
+    id: "root-0",
+    children: [
+      {
+        id: "root-1",
+        module: "dist",
+        collapsed: true,
+        children: [
+          {
+            id: "0",
+            module: "node.js",
+            leaf: true
+          },
+          {
+            id: "1",
+            module: "react-ui-tree.css",
+            leaf: true
+          },
+          {
+            id: "2",
+            module: "react-ui-tree.js",
+            leaf: true
+          },
+          {
+            id: "3",
+            module: "tree.js",
+            leaf: true
+          }
+        ]
+      },
+      {
+        id: "root-2",
+        module: "example",
+        children: [
+          {
+            id: "4",
+            module: "app.js",
+            leaf: true
+          },
+          {
+            id: "5",
+            module: "app.less",
+            leaf: true
+          },
+          {
+            id: "6",
+            module: "index.html",
+            leaf: true
+          }
+        ]
+      },
+      {
+        id: "root-3",
+        module: "lib",
+        children: [
+          {
+            id: "7",
+            module: "node.js",
+            leaf: true
+          },
+          {
+            id: "8",
+            module: "react-ui-tree.js",
+            leaf: true
+          },
+          {
+            id: "9",
+            module: "react-ui-tree.less",
+            leaf: true
+          },
+          {
+            id: "10",
+            module: "tree.js",
+            leaf: true
+          }
+        ]
+      },
+      {
+        id: "11",
+        module: ".gitiignore",
+        leaf: true
+      },
+      {
+        id: "12",
+        module: "index.js",
+        leaf: true
+      },
+      {
+        id: "13",
+        module: "LICENSE",
+        leaf: true
+      },
+      {
+        id: "14",
+        module: "Makefile",
+        leaf: true
+      },
+      {
+        id: "15",
+        module: "package.json",
+        leaf: true
+      },
+      {
+        id: "16",
+        module: "README.md",
+        leaf: true
+      },
+      {
+        id: "17",
+        module: "webpack.config.js",
+        leaf: true
+      }
+    ]
+  };
