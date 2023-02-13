6eda4b71024a3420ceed91594f3213d7325db40c
汪志鹏 <13476859295@163.com>
2023-02-13 23:09:47 +0800

    xxx

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
-import CodeEditor from './components/CodeEditor';
-import FileExplorer from './components/FileExplorer';
-import initialTree from "./data/tree";
+//import { useState } from 'react';
+//import CodeEditor from './components/CodeEditor';
+//import FileExplorer from './components/FileExplorer';
+//import initialTree from "./data/tree";
+//import { FiSettings } from 'react-icons/fi';
 import './App.css';
 function App() {
 
-  const [fileId, setFileId] = useState("0");
-  const [tree, setTree] = useState(initialTree);
+  //const [fileId, setFileId] = useState("0");
+  //const [tree, setTree] = useState(initialTree);
   return (
-    <div>
-      <FileExplorer setFileId={setFileId} tree={tree} setTree={setTree}/>
-      <CodeEditor fileId={fileId} tree={tree} setTree={setTree}/>
+    <div >
+      {/*<FileExplorer setFileId={setFileId} tree={tree} setTree={setTree}/>
+      <CodeEditor fileId={fileId} tree={tree} setTree={setTree}/>*/}
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
