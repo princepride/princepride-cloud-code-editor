22907f8c0e27ae65205c4991f2150038da247ea0
汪志鹏 <13476859295@163.com>
2023-02-16 15:36:37 +0800

    xx

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,11 +4,57 @@ import styles from './Button.module.css'
 import { themeColors } from '../data/dummy';
 import Tooltip from '@mui/material/Tooltip';
 import { BsCheck } from 'react-icons/bs';
+import { slide as Menu } from 'react-burger-menu'
 
 const iconStyle = {
     width: '35px',
     height: '35px',
     display: 'flex'
+  }
+
+  let menuStyles = {
+    bmBurgerButton: {
+      position: 'fixed',
+      width: '36px',
+      height: '30px',
+      left: '36px',
+      top: '36px'
+    },
+    bmBurgerBars: {
+      background: '#373a47'
+    },
+    bmBurgerBarsHover: {
+      background: '#a90000'
+    },
+    bmCrossButton: {
+      height: '24px',
+      width: '24px'
+    },
+    bmCross: {
+      background: '#bdc3c7'
+    },
+    bmMenuWrap: {
+      position: 'fixed',
+      height: '100%'
+    },
+    bmMenu: {
+      background: '#373a47',
+      padding: '2.5em 1.5em 0',
+      fontSize: '1.15em'
+    },
+    bmMorphShape: {
+      fill: '#373a47'
+    },
+    bmItemList: {
+      color: '#b8b7ad',
+      padding: '0.8em'
+    },
+    bmItem: {
+      display: 'inline-block'
+    },
+    bmOverlay: {
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
   }
 
 function Button(props) {
@@ -19,6 +65,24 @@ function Button(props) {
         <button className={styles.buttonStyle} style={{backgroundColor:setting.color}} type="button" onClick={() => {setShow(true)}}>
             <FiSettings style={iconStyle} />
         </button>
+        <Menu width={ '20%' } isOpen={ show } styles={menuStyles} customBurgerIcon={ false } right onClose={()=>setShow(false)}>
+        <p style={{fontSize:'20px', color:setting.theme==='vs-dark'?'white':'black'}}>Theme Colors</p>
+        <div style={{display:'flex', gap:'15px', justifyContent: 'center'}}>
+        {themeColors.map((item, index) => (
+            <Tooltip key={index} title={item.name}>
+            <div key={item.name}>
+                <button
+                type="button"
+                style={{ backgroundColor: item.color, height: '50px', width: '50px', borderRadius: '25px', cursor: 'pointer' }}
+                onClick={() => setsetting({...setting,color:item.color})}
+                >
+                <BsCheck style={{fontSize:'25px', color:'white', visibility: item.color === setting.color ? 'visible' : 'hidden'}}/>
+                </button>
+            </div>
+            </Tooltip>
+        ))}
+        </div>
+        </Menu>
         {/*<Offcanvas show={show} onHide={() => setShow(false)} placement="end" style={{backgroundColor:setting.backgroundColor}}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title style={{fontSize:'25px', fontWeight: 'bold', color:setting.theme==='vs-dark'?'white':'black'}}>Settings</Offcanvas.Title>
