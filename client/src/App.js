67e5428818db9345af2131c95cf41b6baa841c2f
汪志鹏 <13476859295@163.com>
2023-02-19 17:13:45 +0800

    add uuid to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,20 @@
-import { useState, useEffect } from 'react';
-import CodeEditor from './components/CodeEditor';
-import FileExplorer from './components/FileExplorer';
-import initialTree from "./data/tree";
-import OffCanvas from "./components/OffCanvas";
-import SplitPane from "react-split-pane";
-import RunButton from "./components/RunButton";
-import LanguageLabel from "./components/LanguageLabel";
-import './App.css';
-
-// theme:vs-dark => '#1a202c' light => '#ffffff'
-const initialSetting = {
-  color: '#ff0000',
-  theme: "vs-dark",
-  fontSize: [26],
-  language: "javascript"
-} 
+import Canvas from "./components/Canvas"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import {v4 as uuidV4} from 'uuid'
 function App() {
-
-  const [fileId, setFileId] = useState("0");
-  const [tree, setTree] = useState(initialTree);
-  const [setting, setSetting] = useState(initialSetting);
-
-  useEffect(() =>{
-    let element1 = document.querySelector('.file-explorer-tree');
-    let element2 = document.querySelector('body');
-    element1.style.color=setting.color;
-    element1.style.backgroundColor = setting.theme==='vs-dark'?'#1a202c':'#ffffff';
-    element2.style.backgroundColor = setting.theme==='vs-dark'?'#1a202c':'#ffffff';
-  },[setting])
-
   return (
-    <div>
-      <SplitPane
-        split="vertical"
-        minSize={100}
-        maxSize={-100}
-        defaultSize={"15%"}
-        style={{"width":"100vw"}}
-      >
-        <FileExplorer setFileId={setFileId} tree={tree} setTree={setTree} setting={setting}/>
-        <CodeEditor fileId={fileId} tree={tree} setTree={setTree} setSetting={setSetting} setting={setting}/>
-      </SplitPane>
-      <OffCanvas setSetting={setSetting} setting={setting}/>
-      <RunButton setting={setting}/>
-      <LanguageLabel setting={setting}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/editor/${uuidV4()}`}/>} />
+        <Route path="/editor/:id" element={(<Canvas />)}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/components/Canvas.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import CodeEditor from './CodeEditor';
+import FileExplorer from './FileExplorer';
+import initialTree from "../data/tree";
+import OffCanvas from "./OffCanvas";
+import SplitPane from "react-split-pane";
+import RunButton from "./RunButton";
+import LanguageLabel from "./LanguageLabel";
+import './Canvas.css';
+
+// theme:vs-dark => '#1a202c' light => '#ffffff'
+const initialSetting = {
+  color: '#ff0000',
+  theme: "vs-dark",
+  fontSize: [26],
+  language: "javascript"
+} 
+function Canvas() {
+
+  const [fileId, setFileId] = useState("0");
+  const [tree, setTree] = useState(initialTree);
+  const [setting, setSetting] = useState(initialSetting);
+
+  useEffect(() =>{
+    let element1 = document.querySelector('.file-explorer-tree');
+    let element2 = document.querySelector('body');
+    element1.style.color=setting.color;
+    element1.style.backgroundColor = setting.theme==='vs-dark'?'#1a202c':'#ffffff';
+    element2.style.backgroundColor = setting.theme==='vs-dark'?'#1a202c':'#ffffff';
+  },[setting])
+
+  return (
+    <div>
+      <SplitPane
+        split="vertical"
+        minSize={100}
+        maxSize={-100}
+        defaultSize={"15%"}
+        style={{"width":"100vw"}}
+      >
+        <FileExplorer setFileId={setFileId} tree={tree} setTree={setTree} setting={setting}/>
+        <CodeEditor fileId={fileId} tree={tree} setTree={setTree} setSetting={setSetting} setting={setting}/>
+      </SplitPane>
+      <OffCanvas setSetting={setSetting} setting={setting}/>
+      <RunButton setting={setting}/>
+      <LanguageLabel setting={setting}/>
+    </div>
+  );
+}
+
+export default Canvas;
